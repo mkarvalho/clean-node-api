b8c9a78a1642a0657fc5e24dba73cb8c348375bb
Matheus Carvalho <carvalhomath95@gmail.com>
2021-05-22 22:40:06 -0300

    feat: ensure AccoutMongoRespository retuns an account on sucess

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,7 +1,8 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+
   async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
@@ -10,5 +11,8 @@ export const MongoHelper = {
   },
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
