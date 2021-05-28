3240fc16c1ab02a66e7d3944a10bca26c3f59496
Matheus Carvalho <carvalhomath95@gmail.com>
2021-05-27 22:38:17 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'matheus@email.com',
+          password: '123456'
+        })
+        .expect(401)
+    })
   })
 })
