a2ede8472a2fc5cf901e30cf7ada5bf466b52804
Matheus Carvalho <carvalhomath95@gmail.com>
2021-05-31 19:23:04 -0300

    refactor: move login and signup controllers to a folder

--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -1,5 +1,5 @@
-import { MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized, ok } from '../../helpers/http/http-helper'
+import { MissingParamError } from '../../../errors'
+import { badRequest, serverError, unauthorized, ok } from '../../../helpers/http/http-helper'
 import { LoginController } from './login-controller'
 import { HttpRequest, Authentication, Validation, AuthenticationModel } from './login-controller-protocols'
 
